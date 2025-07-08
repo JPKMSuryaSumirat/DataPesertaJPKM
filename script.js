d8f799c28b9a3a05c431fdd5c872abeff65e92cf
JPKMSuryaSumirat <marketingjpkmss@gmail.com>
2025-07-08 10:34:25 +0700

    Update script.js
    Pencarian hanya No JPKM

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ window.addEventListener("DOMContentLoaded", function () {
     event.preventDefault();
 
     const jpkmInput = document.getElementById("nojpkm").value.trim().toUpperCase();
-    const nameInput = document.getElementById("name").value.trim().toLowerCase();
-    const packageInput = document.getElementById("package").value.trim().toUpperCase();
 
     const loadingElement = document.getElementById("loading");
     const resultElement = document.getElementById("result");
@@ -21,8 +19,6 @@ window.addEventListener("DOMContentLoaded", function () {
         const data = json["Sheet1"];
         const peserta = data.find((item) => {
           const matchJPKM = jpkmInput && item["No JPKM"]?.toUpperCase() === jpkmInput;
-          const matchName = nameInput && item["Nama Member"]?.toLowerCase() === nameInput;
-          const matchPackage = packageInput && item["Nama Paket"]?.toUpperCase() === packageInput;
           return matchJPKM || (matchName && matchPackage);
         });
 
